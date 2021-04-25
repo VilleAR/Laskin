39429b68af0abb330ffb0f7bc9672f60bec26621
VilleAR <vilpuurunen@gmail.com>
2021-04-25 23:54:00 +0300

    Found topic, set up databases, figured out layout

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-En ole vielä keksinyt aiheeseen toimivaa ideaa, joten yritin tehdä joitain perusfunktioita ensin. Jostain syystä nekään eivät vielä toimi.
+Inspiraatio iski vasta hiljattain, joten työ on vielä hyvin pahasti vaiheessa. Toivottavasti hidas aloitus ei haittaa lopputuloksen arvostelua.
--- app.py
+++ app.py
@@ -8,26 +8,54 @@ import torch
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+db = SQLAlchemy(app)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
 @app.route("/registration")
 def registration():
     return render_template("registration.html")
+@app.route("/home")
+def home():
+    return render_template("home.html")
 @app.route("/register", methods=["POST"])
 def register():
-    username=request.form["username"]
+    name=request.form["name"]
     password=request.form["password"]
+    print("????")
     hash_value = generate_password_hash(password)
-    sql="INSERT INTO users (username, password) VALUES (:username, :password)"
-    db.session.execute(sql, {"username":username,"password":hash_value})
+    sql="INSERT INTO users (name, password) VALUES (:name, :password)"
+    db.session.execute(sql, {"name":name,"password":hash_value})
     db.session.commit()
     return redirect("/")
-@app.route("/login")
+
+@app.route("/login",methods=["POST"])
 def login():
-    return render
+    name = request.form["name"]
+    password = request.form["password"]
+    sql = "SELECT password FROM users WHERE name=:name"
+    result = db.session.execute(sql, {"name":name})
+    user = result.fetchone()    
+    if user==None:
+        return redirect("/")
+    else:
+        hash_value=user[0]
+        if check_password_hash(hash_value, password):
+            session["name"]=name
+            return redirect("/home")
+        else:
+            return redirect("/")
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    del session["name"]
+    return redirect("/")
 
 @app.route("/calculator")
 def calculator():
